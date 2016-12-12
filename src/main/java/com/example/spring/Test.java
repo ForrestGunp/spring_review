f50ab347c224316c087dbea35eaef468f8af01ae
Tang <446956546@qq.com>
2016-12-12 12:12:16 +0800

    first commit!

--- src/main/java/com/example/spring/Test.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring;
-
-/**
- * Created by Tang on 2016/12/12.
- */
-public class Test {
-}
---
+++ src/main/java/com/example/spring/UnitTest.java
@@ -0,0 +1,13 @@
+package com.example.spring;
+
+import org.junit.Test;
+
+/**
+ * Created by Tang on 2016/12/12.
+ */
+public class UnitTest {
+    @Test
+    public  void test(){
+
+    }
+}
