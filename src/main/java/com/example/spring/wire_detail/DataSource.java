9da8e7665da75fce98c919f462de9ba3598bf7d4
Tang <446956546@qq.com>
2016-12-12 20:03:00 +0800

    spring装配细节总结2

---
+++ src/main/java/com/example/spring/wire_detail/DataSource.java
@@ -0,0 +1,20 @@
+package com.example.spring.wire_detail;
+
+import java.util.Properties;
+
+/**
+ * 模拟数据源属性配置
+ *
+ */
+public class DataSource {
+
+    private Properties properties;
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+}
--- src/main/java/com/example/spring/wire_detail/UnitTest.java
+++ src/main/java/com/example/spring/wire_detail/UnitTest.java
@@ -9,22 +9,32 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class UnitTest {
     @Test
-    public  void  test(){
-        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
-        Person person= (Person) context.getBean("person");
+    public void test() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = (Person) context.getBean("person");
         System.out.println(person);
 
-        Person person2= (Person) context.getBean("person2");
+        Person person2 = (Person) context.getBean("person2");
         System.out.println(person2);
 
 
-        person= (Person) context.getBean("person3");
+        person = (Person) context.getBean("person3");
         System.out.println(person);
 
 //        person= (Person) context.getBean("person4");
 //        System.out.println(person);
 
-       Person2 person4= (Person2) context.getBean("person4");
+        Person2 person4 = (Person2) context.getBean("person4");
         System.out.println(person4);
+
+        DataSource dataSource = (DataSource) context.getBean("dataSource");
+        System.out.println(dataSource.getProperties().get("user"));
+
+        Person2 person5 = (Person2) context.getBean("person5");
+        System.out.println(person5);
+
+        dataSource = (DataSource) context.getBean("dataSource2");
+        System.out.println(dataSource.getProperties().get("url"));
+
     }
 }
